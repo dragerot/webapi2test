08d5b1d8ef735a4c1ae385c88cf3913469aa9b25
Tore Gard <dragerot@gmail.com>
2015-07-18 16:38:44 +0200

    Legge til

---
+++ webapi2test/webapi2azure/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webapi2azure.Models
+{
+    class Customer
+    {
+        int customerId { get; set; }
+        Person personInfo { get; set; }
+    }
+}
