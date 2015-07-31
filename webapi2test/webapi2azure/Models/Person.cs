4c5ff6fcc8890a7cc005836a2bfcabc28c782fc3
Tore Gard <dragerot@gmail.com>
2015-07-31 14:57:44 +0200

    D number check

--- webapi2test/webapi2azure/Models/Person.cs
+++ webapi2test/webapi2azure/Models/Person.cs
@@ -13,5 +13,10 @@ namespace webapi2azure.Models
         public string Pnr { get; set; }
         public string ForName { get; set; }
         public string SureName { get; set; }
+
+        static public bool PnrStartWithD(Person person)
+        {
+            return person.Pnr.ToUpper().StartsWith("D");
+        }
     }
 }
